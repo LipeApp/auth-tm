edc7c22bf86f0e4d2838bdbc04ff04db91702056
Sarvar <sarvar_web@texnomart.uz>
2022-09-21 15:17:18 +0500

    test]

--- routes/auth_tm.php
+++ routes/auth_tm.php
@@ -3,6 +3,6 @@
 
 namespace Seshpulatov\AuthTm\Http\Controllers;
 
-\Illuminate\Support\Facades\Route::get("login", [AuthTmController::class, 'login'])->name('login');
-\Illuminate\Support\Facades\Route::get("logout", [AuthTmController::class, 'logout'])->name('logout');
+\Illuminate\Support\Facades\Route::get("login", [AuthTmController::class, 'login'])->name('tm_login');
+\Illuminate\Support\Facades\Route::get("logout", [AuthTmController::class, 'logout'])->name('tm_logout');
 \Illuminate\Support\Facades\Route::get("login-test", [AuthTmController::class, 'test']);
