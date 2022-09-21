e52fe07988b0afd74f8c0ee204ed1a021b1473d2
Sarvar <sarvar_web@texnomart.uz>
2022-09-21 16:33:01 +0500

    test

--- config/auth_tm.php
+++ config/auth_tm.php
@@ -4,6 +4,7 @@ return [
     'key'   =>  env('AUTH_SERVICE_ID'),
     'login_url' => env('AUTH_SERVICE_URL')."/auth",
     'login_check' => env('AUTH_SERVICE_URL')."/api/check",
+    'logout_url' => env('AUTH_SERVICE_URL')."/api/logout",
     'callback_url'  =>  env('APP_URL').'/login',
     'auth_session_key'  =>  'this-my-auth-key',
     'service_id'        =>  env('AUTH_SERVICE_ID')
